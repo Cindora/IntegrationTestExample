56aa287fcc1aa0214628d090d4808d418f0304b3
Cindora <st111245@student.spbu.ru>
2024-04-30 19:13:29 +0300

    Init commit of Calculator Test

---
+++ src/Test/JointCalculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package Test.JointCalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void calculateFully() {
+
+    }
+}
